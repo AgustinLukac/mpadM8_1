86e84726c49f5f486ff28f0556d73257c90baf69
AgustinLukac <agustin_ruso2003@hotmail.com>
2024-11-23 15:21:54 -0300

    a ver con silvio

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.title("Hola desde strealit cloud")
 #Boton para mostrar el mensaje
 
 if st.button("Mostrar mensaje"):
-    st.write("Ignacio te clava SANTANDER CRISTIAN CON SUS BUBIS")
+    st.write("Ignacio te clava SANTANDER CRISTIAN CON SUS BUBIS creo que con silviolin te pasa")
