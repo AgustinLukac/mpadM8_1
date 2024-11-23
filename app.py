52f7514c7bb34f39e7436fe4edffbda5517e5298
AgustinLukac <agustin_ruso2003@hotmail.com>
2024-11-23 15:55:20 -0300

    Cuando me coji a tronco

--- app.py
+++ app.py
@@ -8,3 +8,11 @@ st.title("Hola desde strealit cloud")
 if st.button("Mostrar mensaje"):
     st.write("Ignacio te clava SANTANDER CRISTIAN CON SUS BUBIS creo que con silviolin te pasa")
     st.write("Ingnacio TEnes cara de contra puto")
+
+    st.title("Incrustar un video de YouTube en Streamlit")
+
+    # URL del video de YouTube
+    video_url = "https://www.youtube.com/watch?v=l91cnF-rrRI"  # Reemplaza con la URL de tu video
+
+    # Mostrar el video en Streamlit
+    st.video(video_url)
